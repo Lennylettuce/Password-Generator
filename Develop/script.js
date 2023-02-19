8eff0509d850fb0df9937e4edfaf88c31b5a4853
Aubrey Gunn <agunn369@gmail.com>
2023-02-19 13:13:02 -0600

    edit .js, add prompts/alert details, comments in code for to-do list

--- Develop/script.js
+++ Develop/script.js
@@ -3,10 +3,37 @@
 // then use forloop and math. to actually generate the password based on these values above
 // make sure to include everything you want processed in this funciton/method wrapped in the curly braces
 //remember to write in return then what you want to return after this computes
+
+//write code that displays options in browser for user
+
+//get range for length and charset array so options display/interact right
+
 function generatePassword(){
-  var length = 8;
+  var options = ["Password length" , "Character type"];
+  var length = [8-128];
   var charset = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var password = "";
+
+  var userChoice = window.prompt("What credentials for password?")
+  //write test conditions
+  if (userChoice.options) {
+    window.alert("You've chosen" + options + ".")
+
+  }
+  var userChoice2 = window.prompt("Choose length of password between 8-128 characters.")
+//write test conditions
+  if (userChoice2.length) {
+    window.alert("You've chosen" + length + ".")
+  } else if (!userChoice2.charset) {
+    window.alert("Please choose again.")
+  }
+
+  var userChoice3 = window.prompt("Which character types included in password?")
+//write test conditions
+  if (userChoice3.charset) {
+    window.alert("You've chosen" + charset + ".")
+  }
+
 
   for (var i = 0, n = charset.length; i<length; ++i) {
     password += charset.charAt(Math.floor(Math.random() * n));
