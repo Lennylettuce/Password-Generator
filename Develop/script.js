8ed36a6581ead789eefb36241af8a91d1573efd4
Aubrey Gunn <agunn369@gmail.com>
2023-02-22 12:15:39 -0600

    new draft named scr.js, much cleaner code, has comments, saved old .js for grading

--- Develop/script.js
+++ Develop/script.js
@@ -10,24 +10,13 @@
 // validate choice.legth within 8-128
 // validate user input, handle extra info in else conditions-returns
 // make sure one char type is selected
-
-
-//write code that displays options in browser for user
- 
-//make it display
-document.getElementById("password").value = password;
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//takes info from prompts and uses the expression in the for loop to generate the random password
 
 // Write password to the #password input
 //takes prompt info and makes the random password
-
-
-function thePrompts(){
-
-  var charset = [];
+//write code that displays options in browser for user
+ 
+ 
   var userChoice = [8];
   var userChoice2 = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+"];
   var userChoice3 = ["1","2","3","4","5","6","7","8","9","0"]
@@ -35,14 +24,16 @@ function thePrompts(){
   var userChoice5 = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
 
-
+//code for prompts
+function thePrompts(){
+  var charset = [];
   //first prompt
   // the parseInt enclosing the first prompt, will make the answer's data type a number
 
-  userChoice = parseInt(prompt("Choose password length."));
+  userChoice = parseInt(prompt("Choose password length. (8-128 characters)"));
  //write test conditions for length in first userChoice question
 
-   if (isNaN(userChoice) || userChoice < 8 || userChoice > 128) {
+  if (isNaN(userChoice) || userChoice < 8 || userChoice > 128) {
     window.alert("Try again!");
     //keep this return to make user try again
     return false;
@@ -50,33 +41,45 @@ function thePrompts(){
   
   //second prompt
   //if test their choice and the concat adds their choice to the chosen empty charset array to be selected from for random password gen.
-  if (userChoice2 = window.confirm("Include special characters?")) {
+  //.concat adds (x) into the z.concat
+  if (confirm("Include special characters?")) {
     charset = charset.concat(userChoice2);
   }
   
   //third prompt
-  if (userChoice3 = window.confirm("Include numerical characters?")) {
+  if (confirm("Include numerical characters?")) {
     charset = charset.concat(userChoice3);
   }
 
   //fourth prompt
-  if (userChoice4 = window.confirm("Include lowercase characters?")) {
+  if (confirm("Include lowercase characters?")) {
     charset = charset.concat(userChoice4);
   }
 
   //fifth prompt
-  if (userChoice5 = window.confirm("Include uppercase characters?")) {
+  if (confirm("Include uppercase characters?")) {
     charset = charset.concat(userChoice5);
   }
   //
   return true;
+};  
+
+
+function writePassword() {
+  var getPrompts = thePrompts();
+  var passwordText = document.querySelector("#password");
+  if(getPrompts){
+  var passwordGen = generatePassword();  
+  passwordText = passwordGen;
+  }
+
 };
 
+  //randomNum to store that random generated value
 
 function generatePassword(){
-
   var password = "";
-  for (var i = 0, n = charset.userChoice; i<userChoice; ++i) {
+  for (var i = 0, i = charset.userChoice; i<userChoice; ++i) {
     var randomNum = Math.floor(Math.random() * charset.userChoice);
     password = password + charset[randomNum]; 
   }
@@ -84,27 +87,24 @@ function generatePassword(){
   return password;
 };
 
-
-function writePassword() {
-  var getPrompts = thePrompts();
-  var passwordText = document.querySelector("#password");
-  var password = generatePassword();
-  
-
-  if (getPrompts) {
-    passwordText.value = password;
-  } else {
-    passwordText.value = "";
-  }
-};
-
-
-//takes info from prompts and uses the expression in the for loop to generate the random password
+//make it display
+document.getElementById("password").value = passwordText;
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 
 
 
-//code for prompts
+
+
+
+
+
+
+
+
 
 
 
